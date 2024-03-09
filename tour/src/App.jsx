b5dbc79451d1de34adafdc1d6b5838e31e948d50
allan <allanrey.macasaquit@gmail.com>
2024-03-09 13:19:36 -0500

    Fetching and Rendering

--- tour/src/App.jsx
+++ tour/src/App.jsx
@@ -1,10 +1,56 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Loading from './Loading';
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+	const [tours, setTours] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	const fetchTours = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setLoading(false);
+			setTours(tours);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	}
+
 	return (
-		<>
-			<div>Tours List</div>
-		</>
+		<section>
+			<div className='title'>
+				<h2>Lists of Available Tours</h2>
+				<div className='title-underline'></div>
+			</div>
+			<div className='tours'>
+				{tours.map(({ id, image, price, info, name }) => (
+					<article className='single-tour' key={id}>
+						<img src={image} alt={name} className='img' />
+						<span className='tour-price'>${price}</span>
+						<div className='tour-info'>
+							<h5>{name}</h5>
+							<p>{info}</p>
+						</div>
+					</article>
+				))}
+			</div>
+		</section>
 	);
 }
 
