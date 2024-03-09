ed266239534c67e4f288cb4a9654ff4232846513
allan <allanrey.macasaquit@gmail.com>
2024-03-09 02:25:18 -0500

    add clear button

--- birthday/src/App.jsx
+++ birthday/src/App.jsx
@@ -5,10 +5,16 @@ import data from './data.js';
 function App() {
 	const [people, setPeople] = useState(data);
 	console.log(people);
+
+	function clear() {
+		setPeople([]);
+	}
 	return (
 		<main>
 			<section className='container'>
-				<h1 className='title'>{people.length} Birthday Reminders</h1>
+				<h1 className='title'>
+					{people.length === 0 ? 'No' : people.length} Birthday Reminders
+				</h1>
 				{people.map(({ id, name, age, image }) => (
 					<div key={id}>
 						<article className='person'>
@@ -20,6 +26,9 @@ function App() {
 						</article>
 					</div>
 				))}
+				<button onClick={clear} type='button' className='btn btn-block'>
+					clear
+				</button>
 			</section>
 		</main>
 	);
