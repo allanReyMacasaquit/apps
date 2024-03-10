cd3de5046995ee5f8cd9ebc7f86e360f34c1816b
allan <allanrey.macasaquit@gmail.com>
2024-03-10 00:01:56 -0500

    Set Index

--- review/src/App.jsx
+++ review/src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import './App.css';
+import reviews from './data';
 
 function App() {
+	const [index, setIndex] = useState(0);
+	const { name, image, job, text } = reviews[index];
+	console.log(name);
 	return (
 		<>
 			<main>
