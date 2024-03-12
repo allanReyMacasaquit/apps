827fdf43dcfb8c3a4de644e7fd65bf11ce387806
allan <allanrey.macasaquit@gmail.com>
2024-03-11 21:22:12 -0400

    Title menu

---
+++ foodMenu/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Title from './components/Title';
+
+function App() {
+	return (
+		<main>
+			<section className='menu'>
+				<Title title='our menu' />
+			</section>
+		</main>
+	);
+}
+
+export default App;
