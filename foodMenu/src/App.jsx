254243a7fd461213135310b87bccbd665a444f86
allan <allanrey.macasaquit@gmail.com>
2024-03-11 21:46:13 -0400

    Render  menu items

--- foodMenu/src/App.jsx
+++ foodMenu/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css';
 import Title from './components/Title';
+import menus from './data';
 
 function App() {
 	return (
 		<main>
 			<section className='menu'>
 				<Title title='our menu' />
+				<div className='section-center'>
+					{menus.map(({ id, title, img, price, desc }) => (
+						<article key={id}>
+							<img src={img} alt={title} className='img' />
+							<div className='item-info'>
+								<header>
+									<h5>{title}</h5>
+									<span className='item-price'>${price}</span>
+								</header>
+								<p className='item-text'>{desc}</p>
+							</div>
+						</article>
+					))}
+				</div>
 			</section>
 		</main>
 	);
