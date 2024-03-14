d82bb3af3f367b7136c48a438b0c9b555770dea1
allan <allanrey.macasaquit@gmail.com>
2024-03-13 22:37:18 -0400

    Initial Set up

---
+++ color_generator/src/App.jsx
@@ -0,0 +1,13 @@
+import ColorList from './components/ColorList';
+import Form from './components/Form';
+
+function App() {
+	return (
+		<>
+			<Form />
+			<ColorList />
+		</>
+	);
+}
+
+export default App;
