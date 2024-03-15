b70dcbc9f2ad04e274e507faffad4ff86e827c66
allan <allanrey.macasaquit@gmail.com>
2024-03-14 23:48:50 -0400

    use Ref

--- navbar/src/components/Navbar.jsx
+++ navbar/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import logo from '../assets/logo.svg';
 import { links } from '../data';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const Navbar = () => {
 	const [showLinks, setShowLinks] = useState(false);
+	const linksContainerRef = useRef(null);
+	const linksRef = useRef(null);
 
 	const toggleLinks = () => {
+		console.log(linksRef.current.getBoundingClientRect());
 		setShowLinks(!showLinks);
 	};
 	return (
@@ -14,21 +17,31 @@ const Navbar = () => {
 			<div className='nav-center'>
 				<div className='nav-header'>
 					<img src={logo} className='logo' alt='logo' />
-					<button className='nav-toggle' onClick={toggleLinks}>
-						<FaBars />
+					<button
+						className='nav-toggle'
+						onClick={toggleLinks} // Set isHovered to false on mouse leave
+					>
+						{showLinks ? <FaTimes /> : <FaBars />}
 					</button>
 				</div>
-				{showLinks && (
-					<div className='links-container'>
-						<ul className='links'>
-							{links.map(({ id, url, text }) => (
-								<li key={id}>
-									<a href={url}>{text}</a>
-								</li>
-							))}
-						</ul>
-					</div>
-				)}
+
+				<div
+					className='links-container'
+					ref={linksContainerRef}
+					style={{
+						height: showLinks
+							? `${linksRef.current.getBoundingClientRect().height}px`
+							: '0px',
+					}}
+				>
+					<ul className='links' ref={linksRef}>
+						{links.map(({ id, url, text }) => (
+							<li key={id}>
+								<a href={url}>{text}</a>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
