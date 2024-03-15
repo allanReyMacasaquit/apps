2667c54787373961ebb0e588d539ba55b9d8dfdd
allan <allanrey.macasaquit@gmail.com>
2024-03-15 10:50:04 -0400

    Side bar template

---
+++ sidebar/src/App.jsx
@@ -0,0 +1,26 @@
+import Home from './components/Home';
+import Modal from './components/Modal';
+import Sidebar from './components/Sidebar';
+// import {
+// 	FaBehance,
+// 	FaFacebook,
+// 	FaLinkedin,
+// 	FaTwitter,
+// 	FaSketch,
+// 	FaHome,
+// 	FaUserFriends,
+// 	FaFolderOpen,
+// 	FaCalendarAlt,
+// 	FaWpforms,
+// } from 'react-icons/fa';
+function App() {
+	return (
+		<main>
+			<Home />
+			<Sidebar />
+			<Modal />
+		</main>
+	);
+}
+
+export default App;
