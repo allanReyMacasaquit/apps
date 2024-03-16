212b6fca9587c9df0af495fcf501120d8ba33593
allan <allanrey.macasaquit@gmail.com>
2024-03-16 18:26:28 -0400

    get and use context

--- strapi/src/App.jsx
+++ strapi/src/App.jsx
@@ -1,4 +1,8 @@
+import { useGlobalContext } from './components/context/appProvider';
+
 function App() {
+	const { isSidebarOpen } = useGlobalContext();
+	console.log(isSidebarOpen);
 	return <main>Strapi</main>;
 }
 
