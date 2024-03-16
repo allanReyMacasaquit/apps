62ddd704d2270fdacfc930d4fd4e57fece39866e
allan <allanrey.macasaquit@gmail.com>
2024-03-16 18:56:57 -0400

    Navbar

--- strapi/src/components/Navbar.jsx
+++ strapi/src/components/Navbar.jsx
@@ -1,4 +1,18 @@
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from '../context/appProvider';
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	const { openSidebar } = useGlobalContext();
+	console.log(openSidebar);
+	return (
+		<nav>
+			<div className='nav-center'>
+				<h3 className='logo'>strapi</h3>
+				<button className='toggle-btn' onClick={openSidebar}>
+					<FaBars />
+				</button>
+			</div>
+		</nav>
+	);
 };
 export default Navbar;
