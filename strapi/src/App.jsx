27666815d9b46a27973a2cc61c88f8c8ca625d94
allan <allanrey.macasaquit@gmail.com>
2024-03-16 18:37:10 -0400

    Components setup

--- strapi/src/App.jsx
+++ strapi/src/App.jsx
@@ -1,9 +1,17 @@
-import { useGlobalContext } from './components/context/appProvider';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import Submenu from './components/Submenu';
 
 function App() {
-	const { isSidebarOpen } = useGlobalContext();
-	console.log(isSidebarOpen);
-	return <main>Strapi</main>;
+	return (
+		<main>
+			<Navbar />
+			<Hero />
+			<Sidebar />
+			<Submenu />
+		</main>
+	);
 }
 
 export default App;
--- strapi/src/main.jsx
+++ strapi/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { AppProvider } from './components/context/appProvider.jsx';
+import { AppProvider } from './context/appProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
