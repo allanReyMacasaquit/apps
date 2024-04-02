fc1c07a2ebd86ca4e4f5043d5f5e4bd975229516
allan <allanrey.macasaquit@gmail.com>
2024-04-02 10:16:50 -0400

    Nav bar Component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,10 @@
+import Navbar from './components/Navbar';
+
 function App() {
 	return (
-		<>
-			<div>
-				<h1 className='text-5xl'>Portfolio</h1>
-			</div>
-		</>
+		<main>
+			<Navbar />
+		</main>
 	);
 }
 
